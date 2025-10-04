916ac75c436e6587ee3dff8a4b603b6029e65eb6
ew42 <evan@craigwall.com>
2025-10-04 13:21:28 -0400

    Working on main

--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 After reading "The Goal" by Eliyahu Goldratt, I want visualize and simulate the process of a factory, then implement lessons from Theory of Constraints(TOC).
 
 ## Structure
-/factory-sim
-  simulator.cpp -- Event loop and time live here
+factory-sim/
+    main.cpp -- Loads config, creates and runs sim
+    simulator.cpp -- Event loop and time live here
+    policy.cpp -- different policies live here, gets called when release_tick
+    model.cpp -- all the structs(event, job, station, ...) live here
--- main.cpp
+++ main.cpp
@@ -4,15 +4,16 @@
 
 using namespace std;
 
-enum class EventType {ARRIVAL, JOB_END, MATERIAL_RELEASE};
-enum class JobStatus {IDLE, ACTIVE, TRAVELING, COMPLETED};
-enum class MachineStatus {IDLE, ACTIVE};
+enum class EventType {SERVICE_END, MATERIAL_TICK};
+enum class JobStatus {IDLE, ACTIVE, COMPLETED};
 
 struct Event {
-	unsigned int jobId;
-	unsigned int machineId;
 	double time;
 	EventType type;
+	int workspaceId; // -1 if MATERIAL_TICK
+	int machineId; // -1 if MATERIAL_TICK
+
+    Event(double t, EventType typ, int jId, int mId) : time(t), type(typ), jobId(jId), machineId(mId) {}
 };
 
 struct Earlier {
@@ -25,7 +26,7 @@ struct Earlier {
 * no, if a.time < b.time
 */
 	bool operator() (const Event& a, const Event& b) {
-		return a.time > b.time;
+        return a.time > b.time;
 	}
 }
 
@@ -68,62 +69,21 @@ struct Job {
 	 *
 	 */
 	unsigned int id;
-	unsigned int workspace; // if job completed workspace = -1(aka 4294967295), if not released = 0
+	unsigned int workspace; 
 	JobStatus status;
 	double releaseTime;
 
 	Job (unsigned int idNum, JobStatus stat, double release)
-		: id(idNum), workspace(1), status(stat), releaseTime(release) {}
+		: id(idNum), workspace(0), status(stat), releaseTime(release) {}
 
-	void arrival() { // workspace - 1 ----job----> workspace
-		status = JobStatus::IDLE;
-	}
-
-	void stageFinished() {
-		workspace++;
-		status = JobStatus::Traveling;
-	}
-
-	void jobCompleted() {
-		workspace = -1; //aka 4294967295
-	}
 
 
 
 };
 
 struct Machine {
-	/*
-	 * Data I want:
-	 * 1) Workspace
-	 * 2) Distribution (mean, std dev)
-	 * 3) Status
-	 * 		ex: idle, active
-	 * 4) id?
-	 *
-	 * Methods I want:
-	 * startJob
-	 * changeStatus
-	 * finishJob?
-	 */
-	unsigned int workspace;
-	NormDist dist;
-	MachineStatus status;
-	// unsigned int id;
-	Machine(unsigned int idNum, NormDist distribution)
-		: workspace(1), dist(distribution), status(MachineStatus::IDLE) {}
-	
-	void startJob() {
-		/*
-		* Machine needs to change status to active
-		* 
-		*/
-		status = MachineStatus::ACTIVE;
-	}
-
-	void finishJob() {
-		status = MachineStatus::IDLE;
-	}
+    int jobId = -1;
+    bool busy = false;
 };
 
 struct Workspace {
@@ -132,18 +92,113 @@ struct Workspace {
 	 * 1) id (in order, i.e. 1 -> 2 -> -> 3)
 	 * 2) Vec of machines
 	 * 3) WIP at Workspace
+     * How do I track busy?
 	 */
-	unsigned int id;
-	vector<Machine> machines;
-	unsigned int WIP;
+	int id;
+	vector<Machine> machines; // Machine contains jobId(int) and busy(bool)
+    int wip;
 
+
+    int finishMachine(int mId) {
+        Machine machine = machines[mId];
+        machine.busy = false;
+        int jId = machine.jobId;
+        machine.jobId = -1;
+        return jId;
+    }    
+    
+    Event startMachine(Job& job) { // should only be called after calling anyIdle
+        job.status = JobStatus::ACTIVE;
+        int machine = machines[findIdle()];
+        machine.busy = true;
+        machine.jobId = job.jobId;
+
+    }
+    
+    bool anyIdle() {
+        for (const Machine& machine : Machines) {
+            if (!machine.busy) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    int findIdle() { // returns index of an idle machine, if none, return -1
+        for (size_t i = 0; i < machines.size(); i++) {
+            if (!machines[i].busy) {
+                return static_cast<int>(i);
+            }
+        }
+        return -1;
+    }
 };
 
 struct Sim {
 	/*
-	* Need to figure out the event loop
+    1) on SERVICE_END, what should it do?
+        - move the finished job to the next workspace and check if it has idle machines, if so schedule new event
+        - check if the current workspace has wip and load if it does, if so schedule new event
+    2) on RELEASE_TICK, what should it do?
+        - call policy ontick and schedule another release_tick
+        
+    methods:
+        run -- loads the first material release, starts running the clock, and going through the heap
+        schedule -- takes an event, puts it into the heap
+        handleEvent -- takes in events and processes, see 1) above for SERVICE_END and 2) for RELEASE_TICK
 
 	*/
 	double now = 0; // time var
 	priority_queue<Event, vector<Event>, Earlier> timeline;
+    vector<Workspace> workspaces;
+    vector<Job> jobs;
+    
+    void run() {
+        Event e(0.0, EventType::MATERIAL_TICK, -1, -1);
+
+        schedule(e);
+        while (timeline.size > 0) {
+            e = timeline.top();
+            timeline.pop();
+            now = e.time;
+            handle_event(e);
+        }
+
+    }
+    
+    void handleEvent(const Event& e) {
+        /*
+         * Question of, should handleEvent update all the Job stuff or should that be dealt
+         * with by the methods of the workspace?
+         *
+         *
+        */
+        switch(e.type) {
+            case EventType::SERVICE_END:
+                int station = workspaces[e.workspaceId];
+                int jId = station.finishMachine(e.machineId);
+                
+                if (station.wip != 0) {
+                    Event newServiceEndEvent = station.startMachine(); // returns an Event
+                    timeline.push(newEvent);
+                }                
+                break;
+            case EventType::MATERIAL_TICK:
+                Workspace firstWS = workspaces[0];
+                if (firstWS.anyIdle()) { // this is where a policy check would go
+                    Job newJob = Job(static_cast<unsigned int>(jobs.size()), JobStatus::IDLE, now);
+                    jobs.push_back(newJob);
+                    Event newReleaseEvent = firstWS.startMachine(jobs.back());
+                    schedule(newReleaseEvent);
+                }
+                break;
+        }
+    }
+    
+    void schedule(const Event& e) {
+        timeline.push(e);
+    }
+    
+    
+    
 };
